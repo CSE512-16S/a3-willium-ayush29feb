09cf9ad27bf76893d2bf7ac11c215330dfb79272
Ayush Saraf <ayush29f@uw.edu>
2016-04-26 08:31:59 -0700

    Refactored answers and questions generation

--- js/chart.js
+++ js/chart.js
@@ -36,65 +36,18 @@ d3.json('../source/data.json', function(error, data) {
 
   // testing constants
   // see source/meta.json for all possible values
-  var party = 'D';
-  var question_id = 'income16';
-  var states = [ 'NY', 'AL', 'WY' ];
-
-  var question = data[party][question_id]['question'];
+  var filters = {
+    states : ['NY', 'AL', 'WY'],
+    party : 'D',
+    question_id : 'income16',
+    exclude_answers : []
+  };
+  
+  var question = data[filters.party][filters.question_id]['question'];
   console.log('Answering the question: ' + question + ' for ' + party + ' in ', states);
 
-  // basic filtering
-  answers = data[party][question_id]['answers'].filter(function(d) {
-    return states.indexOf(d.state) != -1;
-  });
-
-  // roll up data
-  answers = rollup(answers);
-
-  // initialize the graph object
-  graph  = {'nodes': [], 'links': []};
-
-  // Add all the data to graph
-  var zeros = []
-  answers.forEach(function(d) {
-    if(d.value < LOWER_BOUND) {
-      zeros.push({ 'name': d.source })
-    } else {
-      graph.nodes.push({ 'name': d.source });
-      graph.nodes.push({ 'name': d.target });
-      graph.links.push({ 'source': d.source, 'target': d.target, 'value': +d.value });
-    }
-  });
-
-  var nulls = []
-  zeros.forEach(function(zr) {
-    existing = graph.nodes.filter(function(d){
-      return d.name === zr.name
-    });
-    if (!existing.length && nulls.indexOf(zr.name) == -1) {
-      nulls.push(zr.name)
-    }
-  });
-
-  // remove duplicate nodes
-  graph.nodes = d3.keys(d3.nest()
-    .key(function(d) {return d.name; })
-    .map(graph.nodes));
-
-  console.log('nodes', graph.nodes);
-  console.log('nulls', nulls);
-
-  // Switch links source/target from data to index in the nodes
-  graph.links.forEach(function(d, i) {
-    graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
-    graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
-  });
-
-  // now loop through each nodes to make nodes an array of objects
-  // rather than an array of strings
-  graph.nodes.forEach(function (d, i) {
-    graph.nodes[i] = { 'name': d };
-  });
+  var answers = generateAnswers(data, filters);
+  var graph = generateGraph(answers);
 
   sankey
     .nodes(graph.nodes)
@@ -142,23 +95,3 @@ d3.json('../source/data.json', function(error, data) {
       .attr('x', 6 + sankey.nodeWidth())
       .attr('text-anchor', 'start');
 });
-
-// rolls up duplicate values for source/target pairs
-var rollup = function rollup(data) {
-  var output = [];
-
-  data.forEach(function(row) {
-    existing = output.filter(function(d){
-      return (d.source === row.source) && (d.target === row.target)
-    })
-    if (existing.length) {
-      var index = output.indexOf(existing[0]);
-      var currentValue = output[index].value;
-      output[index].value = parseInt(currentValue + row.value, 10);
-    } else {
-      output.push(row);
-    }
-  });
-
-  return output;
-}
---
+++ js/util.js
@@ -0,0 +1,89 @@
+var LOWER_BOUND = 10;
+
+// Rolls up duplicate values for source/target pairs
+var rollup = function rollup(data) {
+  var output = [];
+
+  data.forEach(function(row) {
+    existing = output.filter(function(d) {
+      return (d.source === row.source) && (d.target === row.target);
+    });
+    
+    if (existing.length) {
+      var index = output.indexOf(existing[0]);
+      var currentValue = output[index].value;
+      output[index].value = parseInt(currentValue + row.value, 10);
+    } else {
+      output.push(row);
+    }
+  });
+
+  return output;
+}
+
+/* Pre: Takes in the complete data.json file.
+ * filters : { states, party, question_id, exclude_answers }
+ * Post: Returns answers : {}  TODO: define the answers obejct
+ */
+var generateAnswers = function(data, filters) {
+  // Get all the answers/links and filter the states
+  var answers = data[filters.party][filters.question_id]['answers']
+    .filter(function(d) {
+      return filters.states.indexOf(d.state) != -1;
+    });
+  
+  answers = rollup(answers); // rollup the duplicates
+  return answers;
+}
+
+/* Pre: Takes in a filtered answers object {} TODO: define the answers obejct
+ * Post: Returns a complete graph : { nodes: {}, links: {} }
+ */
+var generateGraph = function(answers) {
+  var graph = { 'nodes' : [], 'links' : [] },
+      zeros = [];
+  
+  answers.forEach(function(d) {
+    if(d.value < LOWER_BOUND) {
+      zeros.push({ 'name' : d.source });
+    } else {
+      graph.nodes.push({ 'name' : d.source });
+      graph.nodes.push({ 'name' : d.target });
+      graph.links.push({
+        'source' : d.source,
+        'target' : d.target,
+        'value' : d.value
+      });
+    }
+  });
+  
+  // TODO: Explain in comment.
+  var nulls = []
+  zeros.forEach(function(zr) {
+    existing = graph.nodes.filter(function(d){
+      return d.name === zr.name
+    });
+    if (!existing.length && nulls.indexOf(zr.name) == -1) {
+      nulls.push(zr.name)
+    }
+  });
+  
+  // Remove duplicates
+  graph.nodes = d3.keys(d3.nest()
+    .key(function(d) {return d.name; })
+    .map(graph.nodes));
+
+  // Switch links source/target from data to index in the nodes
+  graph.links.forEach(function(d, i) {
+    graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
+    graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+  });
+  
+  // now loop through each nodes to make nodes an array of objects
+  // rather than an array of strings
+  graph.nodes.forEach(function (d, i) {
+    graph.nodes[i] = { 'name': d };
+  });
+  
+  return graph;
+}
