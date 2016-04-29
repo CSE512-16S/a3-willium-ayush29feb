ec61812c3cce7114054e4a47fd9e7d920e48fd87
Ayush Saraf <ayush29f@uw.edu>
2016-04-29 02:40:18 -0700

    Basic Colors and enter transition

--- src/js/chart.js
+++ src/js/chart.js
@@ -8,7 +8,7 @@ var margin = { top: config.chart.margin.top, right: config.chart.margin.right, b
   height = config.chart.height - margin.top - margin.bottom;
 
 // set color scale
-var color = d3.scale.category20();
+var color = d3.scale.category10();
 
 // Select and initialize the size of the chart
 var svg = d3.select('#canvas').append('svg')
@@ -46,25 +46,29 @@ export function draw(graph) {
     .append('path')
     .attr('class', 'link');
   // Enter + Update
-  links.attr('d', path)
+  links
+    .attr('d', path)
     .style('stroke-width', function (d) { return Math.max(1, d.dy); })
     .transition()
-      .duration(2000)
-      .attrTween('stroke-dasharray', function() {
+      .duration(1000)
+      .ease('linear')
+      .attrTween('stroke-dasharray', function(abc) {
         var len = this.getTotalLength();
         return function(t) { return (d3.interpolateString("0," + len, len + ",0"))(t) }
-      })
+      });
   
   // Append Tittle
   links.append('title')
     .text(function (d) {
       return d.source.name + ' to ' + d.target.name + ' = ' + d.value;
     });
+  
   // Exit
   links.exit().remove();
-
+   
   // Draw the nodes
-  var nodes = nodesGroup.selectAll('.node').data(graph.nodes, function(o) { return o.meta.target_id + '.' + o.meta.source_rank + '.' + o.value; });
+  var nodes = nodesGroup.selectAll('.node')
+    .data(graph.nodes, function(o) { return o.meta.target_id + '.' + o.meta.source_rank + '.' + o.value; });
   // Enter
   var nodesEnterSelection = nodes.enter()
     .append('g')
@@ -77,36 +81,28 @@ export function draw(graph) {
       return _.isEqual(d.type, 'source') ? -config.chart.node.margin : (sankey.nodeWidth() + config.chart.node.margin);
     })
     .attr('dy', '.35em')
-    .attr('text-anchor', function(d) {
-      return _.isEqual(d.type, 'source') ? 'end' : 'start';
-    })
+    .attr('text-anchor', function(d) { return _.isEqual(d.type, 'source') ? 'end' : 'start'; })
     .attr('transform', null);
+  
   // Enter + Update
   nodes
-    .attr('transform', function (d) {
-      return 'translate(' + d.x + ',' + d.y + ')';
-    });
+    .attr('transform', function (d) { return 'translate(' + d.x + ',' + d.y + ')'; });
   nodes.select('rect')
-    .attr('height', function (d) {
-      return d.dy;
+    .attr('height', function (d) { return d.dy; })
+    .style('fill', function (d) { 
+      console.log(d);
+      if (d.type === 'source') return '#9089BF';
+      return '#BB4747'; // TODO: Color Based on the parties
     })
-    .style('fill', function (d) {
-      return d.color = color(d.name.replace(/ .*/, ''));
-    })
-    .style('stroke', function (d) {
-      return d3.rgb(d.color).darker(2);
-    });
+    .style('stroke', function (d) { return d3.rgb(d.color).darker(2); })
+    .style('stroke-width', 0);
+  
   nodes.select('rect').select('title')
-    .text(function (d) {
-      return d.name;
-    });
+    .text(function (d) { return d.name; });
+  
   nodes.select('text')
-    .attr('y', function (d) {
-      return d.dy / 2;
-    })
-    .text(function (d) {
-      return d.name;
-    });
+    .attr('y', function (d) { return d.dy / 2; })
+    .text(function (d) { return d.name; });
   // Exit
   nodes.exit().remove();
 };
